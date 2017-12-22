454b8ec24c3c658164277eb47029595eddb5dd61
lirong <lirong-dfjk@coamc.com>
2017-12-22 17:32:38 +0800

    文档内容保存body增加p标签

--- src/main/java/com/coamctech/bxloan/manager/service/Impl/DocInfoMngServiceImpl.java
+++ src/main/java/com/coamctech/bxloan/manager/service/Impl/DocInfoMngServiceImpl.java
@@ -143,21 +143,23 @@ public class DocInfoMngServiceImpl implements IDocInfoMngService {
 	}
 	//处理文章
 	private String  handleText(String str){
-		StringBuffer newText = new StringBuffer("<p>");
+		StringBuffer newText = new StringBuffer();
 		//文章是否含有P标签
-		if(str.indexOf("<p>")<0||str.indexOf("</p>")<0||str.indexOf("<")<0){
-			List<String> list = CommonHelper.strToList(str,CommonHelper.NEW_LINE);
-			if(null!=list &&list.size()>0){
-				for(int i = 0; i<list.size();i++){
-					String line = list.get(i);
-					newText.append(line).append("</p>\n<p>");
+		List<String> list = CommonHelper.strToList(str,CommonHelper.NEW_LINE);
+		if(null!=list &&list.size()>0){
+			for(int i = 0; i<list.size();i++){
+				String line = list.get(i);
+				if(!line.startsWith("<p>")){
+					line = "<p>"+line;
 				}
+				if(!line.endsWith("</p>")){
+					line = line+"</p>";
+				}
+				newText.append(line).append("\n");
 			}
-			newText = new StringBuffer(newText.substring(0,newText.lastIndexOf("<p>")));
-			return newText.toString();
-		}else{
-			return str;
 		}
+		return newText.toString();
+
 	}
 
 }
