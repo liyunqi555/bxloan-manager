b24228ef74cd37e4e40ce775b6094e20069f0719
zhaoqingwen <zhaoqingewen@coamc.com>
2017-12-27 22:07:27 +0800

    新增莱姆来源表Dao接口

---
+++ src/main/java/com/coamctech/bxloan/manager/dao/DocColumnDocSourceRelDao.java
@@ -0,0 +1,22 @@
+package com.coamctech.bxloan.manager.dao;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coamctech.bxloan.manager.domain.DocColumn;
+import com.coamctech.bxloan.manager.domain.DocColumnDocSourceRel;
+
+/**   
+ * 类名称：DocColumnDocSourceRelDao<br/>
+ * 类描述 ：<br/>
+ * 创建人: zhaoqingwen<br/>
+ * 创建时间：2017年12月27日 下午9:26:01<br/>  
+ * 修改人：
+ * 修改时间：  
+ * 修改备注：
+ * 版本： V1.0
+ */
+@Repository
+public interface DocColumnDocSourceRelDao extends JpaSpecificationExecutor<DocColumnDocSourceRel>,PagingAndSortingRepository<DocColumnDocSourceRel,Long> {
+}
---
+++ src/main/java/com/coamctech/bxloan/manager/service/VO/ColumnSourceTreeVo.java
@@ -0,0 +1,16 @@
+package com.coamctech.bxloan.manager.service.VO;
+
+/**   
+ * 类名称：ColumnSourceTreeVo<br/>
+ * 类描述 ：<br/>
+ * 创建人: zhaoqingwen<br/>
+ * 创建时间：2017年12月26日 下午8:47:48<br/>  
+ * 修改人：
+ * 修改时间：  
+ * 修改备注：
+ * 版本： V1.0
+ */
+
+public class ColumnSourceTreeVo {
+	
+}
